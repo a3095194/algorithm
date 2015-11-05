8ded0db17fc01ce0a2b191ec859046dc038c8e4d
副島浩太 <a3095194@gmail.com>
2015-11-05 22:31:08 +0900

    Reupload

--- Mondai4.java
+++ Mondai4.java
@@ -1,15 +1,16 @@
 public class Mondai4 {
+
 	public static void main(String[] args) {
 		int m = Integer.parseInt(args[0]);
 		int n = Integer.parseInt(args[1]);
 		int r = 0;
 
-        while( (r = m % n) != 0 ) {
-            m = n;
-            n = r;					//最初のmとnの値を使いたい
-        }
-        System.out.println(m+" "+n);
-        System.out.println((m*n)/n);
+		while( (r = m % n) ! = 0 ) {
+			m = n;
+			n = r;	//最初のmとnの値を使いたい
+		}
+	System.out.println(m + " " + n);
+	System.out.println( (m * n) / n);
 	}
 
 }
--- Mondai5.java
+++ Mondai5.java
@@ -1,9 +1,11 @@
 public class Mondai5 {
+
 	public static void main(String[] args) {
-            int x = Integer.parseInt(args[0]);
-            int fact = 1;
-            for(int i=1; i<=x; i++) fact *= i;
-            System.out.println(x + "! = " + fact);
+		int x = Integer.parseInt(args[0]);
+		int fact = 1;
+
+		for(int i = 1; i < = x; i++) fact * = i;
+			System.out.println(x + "! = " + fact);
 	}
 
 }
