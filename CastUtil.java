8470421525d8dad2ec3a4ba0f5f448e673e8ca01
副島浩太 <a3095194@gmail.com>
2015-12-21 21:33:19 +0900

    syusei

--- CastUtil.java
+++ CastUtil.java
@@ -1,11 +1,11 @@
 package common; 
 class  CastUtil {
   public static int castint (int i) {
-    int i = Integer.parseInt(args[0]);
+    return Integer.parseInt(args[0]);
     return i;
   }
   public static double castdouble (double j) {
-    double j = Double.parseDouble(args[0]);
+    return Double.parseDouble(args[0]);
     return j;
   }
 }
