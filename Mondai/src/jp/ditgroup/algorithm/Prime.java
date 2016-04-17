bdd56d7b84404d4b6373646c48a32c5f3f93da05
副島浩太 <a3095194@gmail.com>
2016-04-17 23:35:23 +0900

    素数判定ロジック全面見直し

--- Mondai/src/jp/ditgroup/algorithm/Prime.java
+++ Mondai/src/jp/ditgroup/algorithm/Prime.java
@@ -5,31 +5,24 @@
  */
 public class Prime {
 	public static void main(String[] args) {
-		getPrime();
+		calculatePrime();
 	}
 
 	/**
 	 * 素数を計算
 	 */
-	public static void getPrime() {
-		int t[] = new int[100];
-		int i, j;
-		// 配列tに2～99までの数字を格納(1は素数とはしないため除外)
-		for (i = 2; i < 100; i++) {
-			t[i] = i;
-		}
-		// 2～8までの倍数を除外する(0をセットする)
-		for (i = 2; i < 8; i++) {
-			for (j = i + i; j < 100; j = j + i) {
-				t[j] = 0;
-			}
-		}
-		for (i = 2; i < 100;) {
-			for (j = 0; j < 10 && i < 100; i++) {
-				if (t[i] != 0) {
-					System.out.print("   " + i);
-					j++;
+	public static void calculatePrime() {
+		boolean flg;
+		for (int i = 2; i <= 100; i++) {
+			flg = true;
+			for (int j = 2; j <= i / 2; j++) {
+				if (i % j == 0) {
+					flg = false;
+					break;
 				}
+			}
+			if (flg) {
+				System.out.println(i);
 			}
 		}
 	}
