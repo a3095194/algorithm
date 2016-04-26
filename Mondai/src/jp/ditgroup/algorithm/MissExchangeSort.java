e8d7be8d7779a2c5a91d9b17eac48fa70122b354
副島浩太 <a3095194@gmail.com>
2016-04-27 00:59:49 +0900

    ExchangeSortの正常に並べ替えが行われないもの

---
+++ Mondai/src/jp/ditgroup/algorithm/MissExchangeSort.java
@@ -0,0 +1,29 @@
+﻿package jp.ditgroup.algorithm;
+
+public class MissExchangeSort {
+	/**
+	 * 結果の配列を出力
+	 */
+	public static void main(String[] args) {
+		int type[] = calculateExchange();
+		for (int i = 0, num = type.length; i < num; i++) {
+			System.out.print(type[i] + " ");
+		}
+	}
+
+	public static int[] calculateExchange() {
+		int orgArray[] = { 35, 26, 48, 34, 32 };
+		int newArray[] = { 35, 26, 48, 34, 32 };
+		for (int i = 0, num = orgArray.length; i < num; i++) {
+			for (int j = i + 1; j < num; j++) {
+				if (orgArray[i] < orgArray[j]) {
+					newArray[i] = orgArray[j];
+					newArray[j] = orgArray[i];
+					orgArray = newArray;
+				}
+			}
+
+		}
+		return newArray;
+	}
+}
